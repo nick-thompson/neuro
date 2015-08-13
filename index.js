a8ee3331720c964dfc5e1a1b6398839f4a0e3e7d
Nick Thompson <ncthom91@gmail.com>
2015-08-13 14:13:27 -0700

    Changing up the bass internals and api a bit

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ var ctx = new AudioContext();
 var bass = new Bass(ctx);
 
 var BPM = 130;
+var notes = [
+  'f1', '__', 'f1', 'f3',
+  'f1', 'f1', 'f3', 'f1',
+  'f1', '__', '__', '__',
+  '__', '__', '__', 'a2'
+];
 
 bass.connect(ctx.destination);
-bass.schedule(BPM, 1, ['f1', '__', 'f1', 'f3',
-                       'f1', 'f1', 'f3', 'f1',
-                       'f1', '__', '__', '__',
-                       '__', '__', '__', 'a2']);
-bass.play();
+bass.play(BPM, 1, notes, function(e) {
+  console.log('done');
+});
