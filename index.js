e25d47262017371034f169b77ec27176c16754ef
Nick Thompson <ncthom91@gmail.com>
2015-08-16 15:22:14 -0700

    Resampling second pass

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var Bass = require('./lib/generators/Bass');
 var RecorderWrapper = require('./lib/util/RecorderWrapper');
-var S2Sampler = require('./lib/generators/S2Sampler');
+var Sampler = require('./lib/generators/Sampler');
+var Group = require('./lib/generators/Group');
 
 var ctx = new AudioContext();
 var bass = new Bass(ctx);
-var recorder = new RecorderWrapper(bass);
+var recorder = new RecorderWrapper(ctx);
 
 var BPM = 130;
 var notes = [
@@ -14,14 +15,35 @@ var notes = [
   '__', '__', '__', 'a2'
 ];
 
+bass.connect(recorder);
 bass.connect(ctx.destination);
 recorder.start();
 
+// Step 1.
 bass.play(BPM, 1, notes, function(e) {
   console.log('Recording finished... collecting buffer.');
   recorder.stop(function(buffer) {
-    var sampler = new S2Sampler(ctx, buffer);
-    sampler.connect(ctx.destination);
-    sampler.start(0);
+
+    // Step 2.
+    var s1 = new Sampler(ctx, buffer);
+    var s2 = new Sampler(ctx, buffer);
+    var gr = new Group(ctx, [s1, s2]);
+    var recorder = new RecorderWrapper(ctx);
+
+    s2.node.detune.value = 3;
+    gr.connect(recorder);
+    gr.connect(ctx.destination);
+
+    recorder.start();
+    gr.play(function(e) {
+      console.log('Recording finished... collecting buffer.');
+      recorder.stop(function(buffer) {
+
+        // Step 3.
+        var s1 = new Sampler(ctx, buffer);
+        s1.connect(ctx.destination);
+        s1.play();
+      });
+    });
   });
 });
