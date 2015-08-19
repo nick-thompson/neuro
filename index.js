14dac51074ac1b9662bb8d4767c0104b9aa32c36
Nick Thompson <ncthom91@gmail.com>
2015-08-19 10:57:43 -0700

    Weighted random filter modulation in step two

--- index.js
+++ index.js
@@ -22,10 +22,36 @@ function play() {
   }
 }
 
+// Returns a random number generator weighted towards the lower end of the
+// provided range.
+function makeRandomGenerator(min, max, weight) {
+  return function rand() {
+    return Math.pow(Math.random(), weight) * (max - min + 1) + min;
+  }
+}
+
+function scheduleFilterAutomation(param, steps, randWeight) {
+  var beats = 4;
+  var duration = (60 / BPM) * beats;
+  var interval = duration / steps;
+  var rand = makeRandomGenerator(40, 18000, randWeight);
+
+  param.setValueAtTime(rand(), ctx.currentTime);
+
+  for (var i = 0; i < steps; i++) {
+    var delta = (i + 1) * interval;
+    var t = ctx.currentTime + delta;
+
+    param.exponentialRampToValueAtTime(rand(), t);
+  }
+}
+
+// Step one is just the raw bass synth sent through a waveshaper and recorded
+// out to a buffer.
 function stepOne(callback) {
   var bass = new Bass(ctx);
   var recorder = new RecorderWrapper(ctx);
-  var ws = new WaveShaper(ctx, {drive: 2.0});
+  var ws = new WaveShaper(ctx, {drive: 2.8});
   var notes = [
     'g#1', 'g#1', '__', 'g#3',
     'g#1', 'g#1', '__', 'b2',
@@ -43,18 +69,48 @@ function stepOne(callback) {
   });
 }
 
+// In step two, we duplicate the incoming buffer and play one duplicate next
+// to the other but detuned +3 cents. We then send both sources through two
+// parallel effects racks. In the first, we have two heavily-modulated filters,
+// followed by a chorus and another distortion module. In the second, we have
+// a distortion module followed by a chorus. The result is then merged and
+// sent through a soft compression, slight EQ, and additional filter modulation
+// before being recorded out to buffer.
 function stepTwo(buffer, callback) {
   var recorder = new RecorderWrapper(ctx);
   var s1 = new Sampler(ctx, buffer);
   var s2 = new Sampler(ctx, buffer, {detune: 3});
-  var gain = ctx.createGain();
+  var bp = new Filter.Bandpass(ctx, {Q: 0.01});
+  var notch = new Filter.Notch(ctx, {Q: 0.5});
+  var ws1 = new WaveShaper(ctx, {drive: 1.8});
+  var ws2 = new WaveShaper(ctx, {drive: 1.2});
+  var m1 = ctx.createGain();
+  var m2 = ctx.createGain();
 
-  gain.gain.value = 0.5;
+  // Merge the duplicate buffers
+  s1.connect(m1);
+  s2.connect(m1);
 
-  s1.connect(gain);
-  s2.connect(gain);
-  gain.connect(recorder.input);
-  gain.connect(ctx.destination);
+  // Connect the left side of the parallel chain
+  m1.connect(bp.input);
+  bp.connect(notch);
+  notch.connect(ws1);
+  ws1.connect(m2);
+
+  // Connect the right side of the parallel chain
+  m1.connect(ws2.input);
+  ws2.connect(m2);
+
+  // Connect the merged result
+  m2.connect(recorder.input);
+  m2.connect(ctx.destination);
+
+  // Adjustments...
+  m1.gain.value = 0.5;
+  m2.gain.value = 0.5;
+
+  scheduleFilterAutomation(bp._filter.frequency, 8, 3);
+  scheduleFilterAutomation(notch._filter.frequency, 16, 2);
 
   recorder.start();
   play(s1, s2, function(e) {
@@ -78,11 +134,23 @@ function stepThree(buffer, callback) {
   });
 }
 
-// Waterfall
-stepOne(function(b1) {
-  stepTwo(b1, function(b2) {
-    stepThree(b2, function() {
-      console.log('Done');
+// Run each step in series.
+function waterfall() {
+  stepOne(function(b1) {
+    stepTwo(b1, function(b2) {
+      // stepThree(b2, function() {
+      //   console.log('Done');
+      // });
     });
   });
+}
+
+// Enable additional playback via spacebar press
+document.addEventListener('keyup', function(e) {
+  if (e.keyCode === 32) {
+    return waterfall();
+  }
 });
+
+// Kick it off.
+waterfall();
