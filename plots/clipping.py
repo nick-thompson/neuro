07b418735778dd8cace9f2721456ed165baff0e4
Nick Thompson <ncthom91@gmail.com>
2015-08-25 12:55:01 -0700

    Will include s-curve though

--- plots/clipping.py
+++ plots/clipping.py
@@ -24,6 +24,9 @@ plt.plot(x_one_one, np.arctan(x_one_one) - (np.arctan(x_one_one) ** 3) / 3, colo
 # Hard clipping
 plt.plot(x_one_one, 0.5 * (abs(x_one_one + 0.85) - abs(x_one_one - 0.85)), color='b')
 
+# S-Curve
+plt.plot(x_one_one, (1 + 4) * x_one_one / (1 + 4 * abs(x_one_one)), color='y')
+
 # Identity
 plt.plot(x_one_one, x_one_one, color='k')
 
@@ -42,6 +45,9 @@ plt.plot(x, y - (y ** 3) / 3, color='c')
 
 # Hard clipping
 plt.plot(x, 0.5 * (abs(y + 0.85) - abs(y - 0.85)), color='b')
+
+# S-Curve
+plt.plot(x, (1 + 4) * y / (1 + 4 * abs(y)), color='y')
 
 # Original
 plt.plot(x, y, color='k')
